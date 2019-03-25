de50eb25c847f57a159d08d3bd801f977fcc8ab4
Lucas Poirier <poirier.dev@gmail.com>
2019-03-25 16:20:14 +0100

    adding method to add a movie and to retrieve the list

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap }  from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import Movie from './movie';
+
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,7 @@ export class MovieService {
     const url = `${this.getMovieUrl}${id}`;
     return this.http.get<Movie>(url).pipe(
       // tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Hero>(`getHero id=${id}`))
+      catchError(this.handleError<Movie>(`getMovie id=${id}`))
     );
   }
 
@@ -33,6 +34,25 @@ export class MovieService {
       // tap(_=> this.log('found movies matching "${term}"')),
       catchError(this.handleError<Movie[]>('searchMovies', []))
     )
+  }
+
+  setWatchlist(idMovie: string): void {
+    let favs = [];
+    if(localStorage.getItem("watchlist")) {
+      favs = JSON.parse(localStorage.getItem("watchlist"));
+    }
+    if(!favs.includes(idMovie)) {
+      favs.push(idMovie);
+    }
+    console.log(favs);
+    localStorage.setItem("watchlist", JSON.stringify(favs))
+  }
+
+  checkWatchlist(idMovie: string): void {
+    let favs = JSON.parse(localStorage.getItem("watchlist"));
+    let response = favs.filter(movie => movie === idMovie);
+    return console.log(response)
+    // return true
   }
 
   private handleError<T> (operation = 'operation', result?: T) {
