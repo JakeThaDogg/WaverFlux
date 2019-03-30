d2aeed9949729340c6d54b8d81edb1d58461911d
Lucas Poirier <poirier.dev@gmail.com>
2019-03-31 00:23:02 +0100

    added methods to change markup with theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+import { GeneralService } from './general.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WaverFlux';
+  const THEME;
+
+  constructor(
+    private generalService: GeneralService,
+  ) {}
+
+  ngOnInit() {
+    this.generalService.setTheme()
+    // this.THEME = this.generalService.getTheme();
+    this.generalService.currentTheme.subscribe(theme => this.THEME = theme);
+    console.log(this.THEME)
+  }
+
+  switchTheme(theme: string): void {
+    this.generalService.switchTheme(theme);
+    // this.THEME = localStorage.getItem("theme");
+  }
 }
